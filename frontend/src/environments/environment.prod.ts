3004886d1bb29ac40ad7caac7f24362aecd7b315
aliciamm99 <aliciamerino99@gmail.com>
2023-01-28 21:29:26 +0100

    Cambiado directorio de salida

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlApi: 'http://localhost:8443/api',
+  secret_key: 'iuhdfHDFISWENoihpfe89423of98239hoi89UHsdfsdfJOAHE98fas2U30RPFW90dfsdf8203Iosujef0sdfsd4r2o3',
+  main_admin: 'a.merinom.2017@alumnos.urjc.es'
 };
