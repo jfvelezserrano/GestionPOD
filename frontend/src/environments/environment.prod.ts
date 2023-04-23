bbf3e120f47092920b8d221ab6b81cea9ef47f45
aliciamm99 <aliciamerino99@gmail.com>
2023-04-23 12:28:41 +0200

    Cambiada ruta produccion en frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlApi: 'http://localhost:8443/api',
+  urlApi: 'https://gestionpod.aliciamm.xyz/api',
   secret_key: 'iuhdfHDFISWENoihpfe89423of98239hoi89UHsdfsdfJOAHE98fas2U30RPFW90dfsdf8203Iosujef0sdfsd4r2o3',
   main_admin: 'aliciaejemplo74@gmail.com'
 };
